cacc1972b88a83b52cf3f4024869d8de04701c3a
Nudyess <to.czyzewski@gmail.com>
2023-08-10 10:13:26 +0200

    added better resolution main banner and fixed mobile navigation from scrolling down

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ const closeMenuBtn = document.querySelector('.nav-mobile__close-btn')
 const showMobileMenu = () => {
 	mobileNav.classList.add('nav-mobile--active')
 	mainBanner.style.filter = 'blur(10.5px)'
+	document.body.style.overflowY = 'hidden'
 }
 
 const closeMobileMenu = () => {
 	mobileNav.classList.remove('nav-mobile--active')
 	mainBanner.style.filter = 'none'
+	document.body.style.overflowY = 'visible';
 }
 
 burgerBtn.addEventListener('click', showMobileMenu)
