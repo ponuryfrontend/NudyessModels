f9a15e76d6ef6f46f6bab2f622da8f6daa92b110
Nudyess <to.czyzewski@gmail.com>
2023-08-14 20:12:05 +0200

    added footer year in copyright & whitening header while scrolling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const mobileNav = document.querySelector('.nav-mobile')
 const closeMenuBtn = document.querySelector('.nav-mobile__close-btn')
+const fixedHeader = document.querySelector('.fixed-top-header')
+const footerYear = document.querySelector('.footer__year')
 
 const showMobileMenu = () => {
 	mobileNav.classList.add('nav-mobile--active')
@@ -12,5 +14,22 @@ const closeMobileMenu = () => {
 	document.body.classList.remove('scroll-lock')
 }
 
+const whiteningHeader = () => {
+	const scrollListener = window.scrollY
+
+	if (scrollListener >= 50) {
+		fixedHeader.classList.add('white-background')
+	} else if (scrollListener < 50) {
+		fixedHeader.classList.remove('white-background')
+	}
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
+window.addEventListener('scroll', whiteningHeader)
 burgerBtn.addEventListener('click', showMobileMenu)
 closeMenuBtn.addEventListener('click', closeMobileMenu)
