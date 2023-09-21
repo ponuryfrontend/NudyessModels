7dc94cd594e363e307bd3fb4d04986d8f5cf0ffa
Nudyess <to.czyzewski@gmail.com>
2023-09-21 14:18:38 +0200

    Added new responsive layout of main model page and some footer changes

--- js/script.js
+++ js/script.js
@@ -2,20 +2,9 @@ const burgerBtn = document.querySelector('.burger-btn')
 const mobileNav = document.querySelector('.nav')
 const closeMenuBtn = document.querySelector('.nav__close-btn')
 const fixedHeader = document.querySelector('.fixed-top-header')
-const dimensionsBtn = document.querySelector('.buttons__dimensions')
-const dimensionsBox = document.querySelector('.main-model')
-const closeDimensionsBtn = document.querySelector('.main-model__close-btn')
+const dimensionsBtn = document.querySelector('.dimensions__button')
+const dimensionsBox = document.querySelector('.dimensions__box')
 const footerYear = document.querySelector('.footer__year')
-
-const showMobileMenu = () => {
-	mobileNav.classList.add('nav--active')
-	document.body.classList.add('lock-scroll')
-}
-
-const closeMobileMenu = () => {
-	mobileNav.classList.remove('nav--active')
-	document.body.classList.remove('lock-scroll')
-}
 
 const whiteningHeader = () => {
 	const scrollListener = window.scrollY
@@ -27,14 +16,18 @@ const whiteningHeader = () => {
 	}
 }
 
-const showDimensions = () => {
-	dimensionsBox.classList.add('main-model--active')
+const showMobileMenu = () => {
+	mobileNav.classList.add('nav--active')
 	document.body.classList.add('lock-scroll')
 }
 
-const hideDimensions = () => {
-	dimensionsBox.classList.remove('main-model--active')
+const closeMobileMenu = () => {
+	mobileNav.classList.remove('nav--active')
 	document.body.classList.remove('lock-scroll')
+}
+
+const runDimensionsBox = () => {
+	dimensionsBox.classList.toggle('dimensions__box--active')
 }
 
 const handleCurrentYear = () => {
@@ -46,5 +39,4 @@ handleCurrentYear()
 window.addEventListener('scroll', whiteningHeader)
 burgerBtn.addEventListener('click', showMobileMenu)
 closeMenuBtn.addEventListener('click', closeMobileMenu)
-dimensionsBtn.addEventListener('click', showDimensions)
-closeDimensionsBtn.addEventListener('click', hideDimensions)
+dimensionsBtn.addEventListener('click', runDimensionsBox)
